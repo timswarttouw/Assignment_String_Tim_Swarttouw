1069376a172f8b71315ade80875c6be807ef9355
timswarttouw <114086317+timswarttouw@users.noreply.github.com>
2022-10-08 10:38:50 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ __winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
 __human_name__ = 'strings'
 
 # Add your code after this line
-player_1 = 'Ruud Gullit'
-player_2 = 'Marco van Basten'
+player_0 = 'Ruud Gullit'
+player_1 = 'Marco van Basten'
 
 goal_0 = 32
 goal_1 = 54
 
-scorers = player_1 + " " + str(goal_0) + ", " + player_2 + " " + str(goal_1)
+scorers = player_0 + " " + str(goal_0) + ", " + player_1 + " " + str(goal_1)
 
-report = player_1 + ' scored in the ' + str(goal_0) + 'nd minute' + '\n' + player_2 + ' scored in the ' + str(goal_1) + 'th minute' 
+report = player_0 + ' scored in the ' + str(goal_0) + 'nd minute' + '\n' + player_1 + ' scored in the ' + str(goal_1) + 'th minute' 
 print(report)
 
 player = 'Ruud Gullit'
-first_name = player[0:4]
-last_name_len = len(player[5:11])
-name_short = player[:1] + ". " + player[5:11]
-chant =  (first_name + "! ") * 3 + first_name + "!"
-good_chant = chant[22:22] != " "
+first_name = player[:player.find(" ")]
+#print(first_name)
+last_name_len = len(player[player.find(" "):-1])
+#print(last_name_len)
+name_short = player[:1] + "." + player[player.find(" "):]
+#print(name_short)
+chant =  (first_name + "! ") * len(player[:player.find(" ")])
+chant = chant[:-1]
+print(chant)
+good_chant = chant[-1] != " "
 print(good_chant)
